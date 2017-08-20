ff6dd8c93188beb28f0e89e48c1acf95ecb919ab
le tuan anh <tuananh0333@gmail.com>
2017-08-20 13:03:19 +0700

    remove comments lines

--- IceCreamShop/PickFlavorViewController.swift
+++ IceCreamShop/PickFlavorViewController.swift
@@ -43,15 +43,7 @@ public class PickFlavorViewController: UIViewController {
 
     loadFlavors()
   }
-//  Alamofire.request("https://httpbin.org/get").response { response in
-//  print("Request: \(response.request)")
-//  print("Response: \(response.response)")
-//  print("Error: \(response.error)")
-//
-//  if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
-//  print("Data: \(utf8Text)")
-//  }
-//  }
+
   fileprivate func loadFlavors() {
     showLoadingHUD()
     Alamofire.request(
